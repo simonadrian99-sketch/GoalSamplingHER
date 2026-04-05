0bf7f8255c2e970c4b2a8a702dc2e174be10d7b4
Adrian Simon <simon.adrian99@gmail.com>
2026-04-05 14:56:41 +0200

    Added Heatmap Functionality for default HerBuffer

--- her_novelty_buffer.py
+++ her_novelty_buffer.py
@@ -74,71 +74,8 @@ class her_novelty_buffer(HerReplayBuffer):
 
         return new_goals
 
-    """def sample(self, batch_size, env):
-        return self._sample_transitions(batch_size, maybe_vec_env=env)
 
-    def _sample_transitions(self, batch_size, maybe_vec_env):
-
-       
-
-
-        obs = {key: np.copy(v) for key, v in obs.items()}
-        next_obs = {key: np.copy(v) for key, v in next_obs.items()}
-
-        her_indices = np.where(np.random.rand(batch_size) < self.her_ratio)[0]
-
-        for idx in her_indices:
-            ep_idx = episode_indices[idx]
-            t_idx = trans_indices[idx]
-            future_indices = np.arange(t_idx + 1, self.episode_lengths[ep_idx])
-
-            if len(future_indices) > 0:
-                candidate_goals = self.observations['achieved_goal'][ep_idx,
-                                                                     future_indices]
-
-                # debug
-                print(f"DEBUG: Raw Goals Shape: {candidate_goals.shape}")
-                print(f"DEBUG: Erste 3 Goals: {candidate_goals[:3]}")
-
-                coords = candidate_goals.astype(int)
-
-                mask = (coords[:, 0] > 0) & (coords[:, 0] < 9) & \
-                       (coords[:, 1] > 0) & (coords[:, 1] < 9)
-
-                if np.any(mask):
-                    valid_coords = coords[mask]
-                    valid_future_indices = future_indices[mask]
-
-                    counts = self.visit_counts[valid_coords[:,
-                                                            0], valid_coords[:, 1]]
-
-                    best_local_idx = np.argmin(counts)
-                    best_future_idx = valid_future_indices[best_local_idx]
-
-                    new_goal = self.observations['achieved_goal'][ep_idx,
-                                                                  best_future_idx]
-                    obs['desired_goal'][idx] = new_goal
-                    next_obs['desired_goal'][idx] = new_goal
-
-                    if np.random.rand() < 0.05:
-                        print(f"DEBUG Novelty: Aus {len(valid_coords)} Zielen gewählt. "
-                              f"Min Visits: {np.min(counts)}, Max Visits: {np.max(counts)}")
-                else:
-                    if np.random.rand() < 0.05:
-                        print(
-                            f"DEBUG Novelty: default goal used, da kein gültiges Ziel gefunden wurde.")
-
-                    new_goal = self.observations['achieved_goal'][ep_idx,
-                                                                  future_indices[-1]]
-                    obs['desired_goal'][idx] = new_goal
-                    next_obs['desired_goal'][idx] = new_goal
-
-        rewards = maybe_vec_env.env_method(
-            "compute_reward",
-            next_obs['achieved_goal'],
-            next_obs['desired_goal'],
-            [{} for _ in range(batch_size)],
-            indices=range(batch_size)
-        )
-        rewards = np.array(rewards, dtype=np.float32)
-        return self._preprocess_samples(obs, next_obs, rewards, episode_indices, trans_indices)"""
+class her_passive_logger_buffer(HerReplayBuffer):
+    def __init__(self, *args, grid_size=(10, 10), **kwargs):
+        super().__init__(*args, **kwargs)
+        self.visit_counts = np.ones(grid_size, dtype=np.float32)

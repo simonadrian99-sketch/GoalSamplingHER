846b282baac7095cb4383209969575884be82054
Adrian Simon <simon.adrian99@gmail.com>
2026-04-05 14:58:14 +0200

    Added more Case seperation for NoveltyBuffer

--- dqn_agent.py
+++ dqn_agent.py
@@ -11,14 +11,17 @@ import numpy as np
 from minigrid.wrappers import ImgObsWrapper, DictObservationSpaceWrapper
 from stable_baselines3 import A2C, DQN, HerReplayBuffer
 from her_novelty_buffer import her_novelty_buffer as HerNoveltyBuffer
+from her_novelty_buffer import her_passive_logger_buffer as HerPassiveLoggerBuffer
 from stable_baselines3.her.goal_selection_strategy import GoalSelectionStrategy
 import os
 import time
 
 
 LOG_ROOT = "logs"
+MODEL_ROOT = "models"
 ALGORITHM_NAME = "DQN+HER"
-GOAL_SELECTION_STRATEGY = "future"  # "final", "episode", "random", "future"
+GOAL_SELECTION_STRATEGY = "episode"  # "final", "episode", "random", "future"
+NOVELTY_ON = False
 TIMESTAMP = time.strftime("%Y%m%d-%H%M%S")
 
 
@@ -26,20 +29,40 @@ policy_kwargs = dict(
     features_extractor_class=MinigridFeaturesExtractor,
     features_extractor_kwargs=dict(features_dim=512),
 )
-replay_buffer_kwargs = dict(
-    grid_size=(10, 10),
-    n_sampled_goal=8,
-    goal_selection_strategy=GOAL_SELECTION_STRATEGY,
-)
+
+if NOVELTY_ON:
+    replay_buffer_class = HerNoveltyBuffer
+    replay_buffer_kwargs = dict(
+        grid_size=(10, 10),
+        n_sampled_goal=8,
+        goal_selection_strategy=GOAL_SELECTION_STRATEGY,
+    )
+else:
+    replay_buffer_class = HerPassiveLoggerBuffer
+    replay_buffer_kwargs = dict(
+        n_sampled_goal=8,
+        goal_selection_strategy=GOAL_SELECTION_STRATEGY,
+    )
 
 
 # create environment
 env = SimpleEnv(render_mode="rgb_array")
 
 # create model path based on environment size and goal selection strategy
-models_dir = f"models/DQN+HER_{env.width-2}x{env.height-2}_{GOAL_SELECTION_STRATEGY}_{env.GOAL_TYPE}_{TIMESTAMP}"
-LOG_DIR = os.path.join(
-    LOG_ROOT, f"{env.width-2}x{env.height-2}", ALGORITHM_NAME, GOAL_SELECTION_STRATEGY)
+if NOVELTY_ON == False:
+    # models_dir = f"models/DQN+HER_{env.width-2}x{env.height-2}_{env.START_POS_TYPE}_{GOAL_SELECTION_STRATEGY}_{env.GOAL_TYPE}_{TIMESTAMP}"
+    models_dir = os.path.join(MODEL_ROOT, ALGORITHM_NAME, f"{env.width-2}x{env.height-2}_{env.START_POS_TYPE}",
+                              GOAL_SELECTION_STRATEGY, env.GOAL_TYPE, TIMESTAMP)
+    LOG_DIR = os.path.join(
+        LOG_ROOT, f"{env.width-2}x{env.height-2}_{env.START_POS_TYPE}", ALGORITHM_NAME, GOAL_SELECTION_STRATEGY
+    )
+else:
+    LOG_DIR = os.path.join(
+        LOG_ROOT, f"{env.width-2}x{env.height-2}_{env.START_POS_TYPE}", ALGORITHM_NAME, "novelty"
+    )
+    # models_dir = f"models/DQN+HER_{env.width-2}x{env.height-2}_{env.START_POS_TYPE}_novelty_{env.GOAL_TYPE}_{TIMESTAMP}"
+    models_dir = os.path.join(MODEL_ROOT, ALGORITHM_NAME,
+                              f"{env.width-2}x{env.height-2}_{env.START_POS_TYPE}", "novelty", env.GOAL_TYPE, TIMESTAMP)
 
 
 # creating model and log directory
@@ -54,7 +77,7 @@ if not os.path.exists(LOG_ROOT):
 model = DQN(
     "MultiInputPolicy",
     env,
-    replay_buffer_class=HerNoveltyBuffer,
+    replay_buffer_class=replay_buffer_class,
     replay_buffer_kwargs=replay_buffer_kwargs,
     policy_kwargs=policy_kwargs,
     batch_size=512,
@@ -68,36 +91,40 @@ model = DQN(
     max_grad_norm=1
 )
 
-TIMESTEPS = 10000
+TIMESTEPS = 30000
 RUN_NAME = f"run_{TIMESTAMP}"
 
-
+callbacks = []
 action_cb = ActionLoggerCallback()
+callbacks.append(action_cb)
 collision_cb = CollisionLoggerCallback()
+callbacks.append(collision_cb)
 heatmap_cb = HeatmapLoggerCallback(model.replay_buffer)
+callbacks.append(heatmap_cb)
 
 
 # training loop
-for i in range(1, 6):  # 10 Meilensteine à 30k Schritte
+for i in range(1, 11):  # 10 Meilensteine à 30k Schritte
     try:
         # train model 30k steps and log to tensorboard
+        """print(RUN_NAME)
         print("--- DIAGNOSE START ---")
         print(f"Learning Starts: {model.learning_starts}")
         print(f"Train Freq: {model.train_freq}")
         print(f"Total Timesteps: {TIMESTEPS}")
         print(f"Buffer Typ: {type(model.replay_buffer)}")
-        print("--- DIAGNOSE ENDE ---")
+        print("--- DIAGNOSE ENDE ---")"""
         model.learn(
             total_timesteps=TIMESTEPS,
             reset_num_timesteps=False,
-            callback=[action_cb, collision_cb, heatmap_cb],
+            callback=callbacks,
             tb_log_name=RUN_NAME
         )
 
         # save model checkpoint every 10k steps
         save_path = f"{models_dir}/{TIMESTEPS * i}"
         model.save(save_path)
-        np.save("logs/novelty_heatmap_final.npy",
+        np.save(f"{LOG_DIR}/{RUN_NAME}_0/heatmap_{TIMESTEPS * i}.npy",
                 model.replay_buffer.visit_counts)
 
         print(
